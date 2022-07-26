71c7c67bc040ce162d01d2e05046dd99b5a793a8
Antony D'souza <91648718+AntonyDsouza1306@users.noreply.github.com>
2022-07-26 17:21:42 +0530

    Add files via upload

--- Model_building/model_building.py
+++ Model_building/model_building.py
@@ -25,13 +25,13 @@ df = df[~(df.index.year == 2017)]
 #df1 = df[df.index.year > 2011]
 
 #train and test split
-train1 = df.iloc[:516, 0]
-test1 = df.iloc[516:, 0]
+train1 = df.iloc[:528, 0]
+test1 = df.iloc[528:, 0]
 
 train = np.asarray(train1)
-train = np.reshape(train, (516, 1))
+train = np.reshape(train, (528, 1))
 test = np.asarray(test1)
-test = np.reshape(test, (24, 1))
+test = np.reshape(test, (12, 1))
 
 # Normalization
 scaler = MinMaxScaler()
@@ -42,7 +42,7 @@ scaled_test = scaler.transform(test)
 
 
 # Time Series Generator
-n_input = 24
+n_input = 12
 n_features = 1
 generator = TimeseriesGenerator(scaled_train, scaled_train, length=n_input, batch_size=1)
 
